3d317e50df9579f6b21107c27fbedb9bbe8bd98d
Eddy Hernandez <eddy.hernandez@rackspace.com>
2015-06-17 21:46:23 -0700

    Update 2015-06-18T04:46:21.377Z

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,1 @@
-(function(window, document, undefined) {
-  if(document.querySelector) {
-    var codeBlocks = document.querySelectorAll('.content > pre');
-
-    for (var i = 0, len = codeBlocks.length; i < len; i++) {
-      codeBlocks[i].classList.add('stretch');
-    }
-  }
-})(window, document);
+!function(e,t,r){if(t.querySelector)for(var c=t.querySelectorAll(".content > pre"),n=0,o=c.length;o>n;n++)c[n].classList.add("stretch")}(window,document);
